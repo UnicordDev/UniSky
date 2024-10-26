e296ebb0bf9597015c93d1fd6dcc454349bd7b7f
Thomas May <wamwoowam@gmail.com>
2024-10-26 20:27:16 +0100

    uno conversion

---
+++ UniSky/UniSky.Shared/RootPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using CommunityToolkit.Mvvm.DependencyInjection;
+#if WINDOWS_UWP
+using UniSky.Helpers.Composition;
+#endif
+using UniSky.Pages;
+using UniSky.Services;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UniSky;
+
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class RootPage : Page
+{
+    public RootPage()
+    {
+        this.InitializeComponent();
+        Loaded += RootPage_Loaded;
+    }
+
+    private void RootPage_Loaded(object sender, RoutedEventArgs e)
+    {
+        var serviceLocator = Ioc.Default.GetRequiredService<INavigationServiceLocator>();
+        var service = serviceLocator.GetNavigationService("Root");
+        service.Frame = RootFrame;
+
+        var sessionService = Ioc.Default.GetRequiredService<SessionService>();
+        if (ApplicationData.Current.LocalSettings.Values.TryGetValue("LastUsedUser", out var userObj) &&
+            userObj is string user &&
+            sessionService.TryFindSession(user, out var session))
+        {
+            service.Navigate<HomePage>(user);
+        }
+        else
+        {
+            service.Navigate<LoginPage>();
+        }
+
+#if WINDOWS_UWP
+        BirdAnimation.RunBirdAnimation(RootFrame);
+#endif
+    }
+}
---
+++ UniSky/UniSky.Shared/ViewModels/LoginViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.ServiceModel;
+using System.Threading;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using CommunityToolkit.Mvvm.Input;
+using FishyFlip;
+using FishyFlip.Models;
+using Microsoft.Extensions.Logging;
+using UniSky.Extensions;
+using UniSky.Helpers;
+using UniSky.Models;
+using UniSky.Pages;
+using UniSky.Services;
+using UniSky.ViewModels.Error;
+using Windows.UI.Popups;
+
+namespace UniSky.ViewModels;
+
+public partial class LoginViewModel : ViewModelBase
+{
+    private readonly LoginService loginService;
+    private readonly SessionService sessionService;
+    private readonly INavigationService navigationService;
+    private readonly ILogger<ATProtocol> atProtoLogger;
+
+    [ObservableProperty]
+    private bool _advanced;
+    [ObservableProperty]
+    private string _username;
+    [ObservableProperty]
+    private string _password;
+    [ObservableProperty]
+    private string _host;
+
+    public LoginViewModel(
+        LoginService loginService, 
+        SessionService sessionService, 
+        INavigationServiceLocator navigationServiceLocator,
+        ILogger<ATProtocol> atProtoLogger)
+    {
+        this.loginService = loginService;
+        this.sessionService = sessionService;
+        this.navigationService = navigationServiceLocator.GetNavigationService("Root");
+        this.atProtoLogger = atProtoLogger;
+
+        Advanced = false;
+        Username = "";
+        Password = "";
+        Host = "https://bsky.social";
+    }
+
+    [RelayCommand]
+    private async Task Login()
+    {
+        Error = null;
+
+        using var context = GetLoadingContext();
+        try
+        {
+            var normalisedHost = new UriBuilder(Host)
+                .Host.ToLowerInvariant();
+
+            var builder = new ATProtocolBuilder()
+                .EnableAutoRenewSession(true)
+                .WithInstanceUrl(new Uri(Host));
+
+            using var protocol = builder.Build();
+            var session = await protocol.AuthenticateWithPasswordAsync(Username, Password, CancellationToken.None)
+                .ConfigureAwait(false);
+
+            var loginModel = this.loginService.SaveLogin(normalisedHost, Username, Password);
+            var sessionModel = new SessionModel(true, normalisedHost, session);
+
+            sessionService.SaveSession(sessionModel);
+            syncContext.Post(() =>
+                navigationService.Navigate<HomePage>(session.Did));
+        }
+        catch (Exception ex)
+        {
+            syncContext.Post(() =>
+            {
+                Error = new ExceptionViewModel(ex);
+            });
+        }
+    }
+
+    [RelayCommand]
+    private void ToggleAdvanced()
+    {
+        Advanced = !Advanced;
+    }
+}
