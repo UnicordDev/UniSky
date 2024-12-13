e810d5c621af1e67ef908f898d7359700bd12972
Thomas May <wamwoowam@gmail.com>
2024-12-13 00:36:22 +0000

    Add GenericOverlayControl

---
+++ UniSky/Controls/Overlay/GenericOverlayControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace UniSky.Controls.Overlay;
+
+public class GenericOverlayControl : OverlayControl
+{
+    public GenericOverlayControl()
+    {
+        this.DefaultStyleKey = typeof(GenericOverlayControl);
+    }
+
+    protected override void OnShown(RoutedEventArgs args)
+    {
+        base.OnShown(args);
+    }
+
+    protected override void OnHidden(RoutedEventArgs args)
+    {
+        base.OnHidden(args);
+    }
+}
